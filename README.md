046ef5c672879e36c9bbe097e3bbaea36ced3552
勤硕 <qinshuo.lqs@alibaba-inc.com>
2018-12-15 02:19:48 +0800

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+# 💩 The Holy Shitty Network.
+
+I can't endure the 💩 network anymore! (🙀 when the network breaks down...)
+
+```js
+npm start [url url...]
+```
+
+这特么网络环境太特么的恶劣了...该去重启一下路由了 🤦‍
---
+++ index.js
@@ -0,0 +1,46 @@
+const http = require('http');
+const player = require('play-sound')();
+
+const webSite = process.argv.length > 2 ? process.argv.splice(2) : '';
+
+function alert() {
+    player.play('ahh.wav', function(err) {
+        if (err) throw err;
+    });
+}
+
+function unexceptionHandler(e) {
+    console.log(e);
+    this.socket && this.socket.destroy();
+}
+
+function ping(host) {
+    const startTime = new Date();
+    http.get(
+        {
+            host,
+            timeout: 3000,
+        },
+        res => {
+            const endTime = new Date();
+            const spendTime = endTime - startTime;
+            const [s] = res.statusCode.toString();
+            if (s === '2' || s === '3') {
+                console.log(`${host}: ${spendTime}ms`);
+            } else {
+                unexceptionHandler(res);
+            }
+        }
+    )
+        .on('error', unexceptionHandler)
+        .setTimeout(3000, unexceptionHandler);
+}
+
+let counter = 1;
+setInterval(() => {
+    process.stdout.write('\x1Bc');
+    console.log(` ${counter++ * 3}s, It still nice...`);
+    webSite.forEach(site => {
+        ping(site);
+    });
+}, 3000);
